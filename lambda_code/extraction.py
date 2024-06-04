1b37a71dbaf0c257a0e3bf6eb3ebc263aa1cffd6
willywairagu <wwairagu@sanku.com>
2024-06-04 20:01:30 +0300

    injestion code updates

--- lambda_code/extraction.py
+++ lambda_code/extraction.py
@@ -3,8 +3,10 @@ import logging
 import requests 
 import datetime
 import pandas as pd
+import s3_file_operations as s3_ops
 
 url = 'https://api.coincap.io/v2/exchanges'
+bucket = 'de-masterclass'
 
 def lambda_handler(event, context):
     # TODO implement
@@ -13,8 +15,9 @@ def lambda_handler(event, context):
         
     except requests.ConnectionError as ce:
         logging.error(f"There was an error with the request, {ce}")
-
-    data =  pd.DataFrame(r.json().get('data', []))
+    
+    # Convert the json response to a pandas DataFrame
+    exchange_data =  pd.DataFrame(r.json().get('data', []))
 
     # Save Data to s3
     # Get the current timestamp
@@ -27,13 +30,16 @@ def lambda_handler(event, context):
     date = str(timestamp.day).zfill(2)  
     hour = str(timestamp.hour).zfill(2) 
     minute = str(timestamp.minute).zfill(2)
-
     
-
+    # Write exchanges_data to S3
+    s3_ops.write_data_to_s3(exchange_data,
+                            bucket_name=bucket,
+                            key=f"exchanges/{year}/{month}/{date}/{hour}/{minute}/Exchanges.csv")
     
-    print(data.shape)
+    
+    print(f"Successfully Extracted:, {exchange_data.shape[0]} Exchange Records.....")
     
     return {
         'statusCode': 200,
-        'body': json.dumps('Data Successfully Extracted!')
+        'body': json.dumps('Data Successfully Extracted and Saved to AWS S3!')
     }
